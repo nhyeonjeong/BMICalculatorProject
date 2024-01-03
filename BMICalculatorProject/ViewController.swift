4e68a59bdf1306a712806f94d9727b92e14346eb
nhyeonjeong <hjnam5962@gmail.com>
2024-01-04 02:43:16 +0900

    Initial Commit

---
+++ BMICalculatorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculatorProject
+//
+//  Created by 남현정 on 2024/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
